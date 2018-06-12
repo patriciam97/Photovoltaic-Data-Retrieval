87a43861cab0e73ca620bf5a184a0d6c47f9dd89
patriciam97 <patriciamilou97@gmail.com>
2018-06-12 14:05:06 +0300

    url extraction succeed

--- WebCrawler1/src/main/java/Database.java
+++ WebCrawler1/src/main/java/Database.java
@@ -21,21 +21,40 @@ public class Database {
 
 	public static void main(String[] args) throws IOException {
 		// connecting to the database
+		String line=null;
+		String username=null,password=null,host=null,port=null,database=null;
+		String segments[];
 		BufferedReader br = new BufferedReader(new FileReader("configurations.txt"));
-		String dburi=br.readLine(); //reads the first line of the configuration.txt
+		 while((line = br.readLine()) != null) {
+			 segments = line.split(":");
+
+			 if (segments[0].equals("username")){
+				 username=segments[1];
+			 }else if (segments[0].equals("password")){
+				 password=segments[1];
+			 }else if (segments[0].equals("host")){
+				 host=segments[1];
+			 }else if (segments[0].equals("port")){
+				 port=segments[1];
+			 } else if (segments[0].equals("database")){
+				 database=segments[1];
+			 }
+         }   
+		 String dburi="mongodb://"+username+":"+password+"@"+host+":"+port+"/"+database;
+		 br.close(); 
 		MongoClientURI uri=new MongoClientURI(dburi);
 		MongoClient mongoClient = new MongoClient(uri);
 		DB db = mongoClient.getDB("sunnyportal");
-		DBCollection collection= db.getCollection("test");
-		List<Integer> books = Arrays.asList(27464, 747854);
-		DBObject person = new BasicDBObject("_id", "jo3")
-		                            .append("name", "Jo Bloggs")
-		                            .append("address", new BasicDBObject("street", "123 Fake St")
-		                                                         .append("city", "Faketon")
-		                                                         .append("state", "MA")
-		                                                         .append("zip", 12345))
-		                            .append("books", books);
-		collection.insert(person);
+//		DBCollection collection= db.getCollection("test");
+//		List<Integer> books = Arrays.asList(27464, 747854);
+//		DBObject person = new BasicDBObject("_id", "jo3")
+//		                            .append("name", "Jo Bloggs")
+//		                            .append("address", new BasicDBObject("street", "123 Fake St")
+//		                                                         .append("city", "Faketon")
+//		                                                         .append("state", "MA")
+//		                                                         .append("zip", 12345))
+//		                            .append("books", books);
+//		collection.insert(person);
 	}
 
 }
--- WebCrawler1/src/main/java/ExtractInformation.java
+++ WebCrawler1/src/main/java/ExtractInformation.java
@@ -1,43 +1,61 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class ExtractInformation {
-
-	public static void main(String[] args) throws InterruptedException,
-			IOException {
-
-		String doclink = "https://www.sunnyportal.com"; // domain name of the
-														// webpage
-		Document doc = null;
-		doc = Jsoup.connect(doclink).get(); // creates a new connection,fetches
-											// and parses the HTML file
+	private static Document doc = null;
+	public static ArrayList<String> urls= new ArrayList<String>();
+	public static void main(String[] args) throws IOException, InterruptedException{
+		Connect();
 		Thread.sleep(4000);
-		Elements newsHeadlines = doc
-				.select("#ctl00_ContentPlaceHolder1_HyperLinkPublicPlants");
-		for (Element headline : newsHeadlines) {
-			doclink = headline.absUrl("href");
-		}
+		ExtractUrls();
+		urls=ManipulateUrls(); //path to each PV System Profile
+//		for (int i=0;i<urls.size();i++){
+//		System.out.println(urls.get(i));
+//		}
+
+	}
+	public static void Connect() {
+		//connects to the website 
+		String doclink = "https://www.sunnyportal.com/Templates/publicPagesPlantList.aspx"; //webpage
 		try {
-			doc = Jsoup.connect(doclink).get(); // connecting to the website
-												// with the infomation we're
-												// interested
-			Thread.sleep(4000);
+			doc = Jsoup.connect(doclink).get();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(doc.title());
-		Elements reportContent = doc
-				.select("table[class=base-grid] tr td  	a[href^=javascript:OpenPlant(]");
-		for (Element headline : reportContent) {
-			System.out.printf("%s %s \n", headline.text(),
-					headline.absUrl("href"));
+		
+	}
+	public static void ExtractUrls(){
+		//extracts the url for each PV System Profile
+		//Select all elements with an href tag
+		Elements reportContent = doc.select("table[class=base-grid] tr td  	a[href]"); 
+		int urlssize= reportContent.size();
+		//Add all hrefs in an ArrayList called urls
+		for (int i=0;i<urlssize;i++) {
+			Element el=reportContent.get(i);
+			String link=el.attr("href").toString();
+			urls.add(link);
 		}
 
 	}
+	public static ArrayList<String> ManipulateUrls(){
+		//extracts the path for each PV System Profile
+		ArrayList<String> updatedUrls= new ArrayList<String>();
+		String substrings[]=null;
+		for (int i=0;i<urls.size();i++) {
+			if (urls.get(i).toString().contains("OpenPlant")){	
+				//splits the original url we have extracted at these characters
+				substrings=urls.get(i).split("[?']"); 
+				//substring[2] corresponds to the one we want
+				updatedUrls.add(substrings[2]);
+			}
+		}
+		return updatedUrls;
+	}
+	
 }
